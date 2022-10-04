444daa593ccdd4d94cfdc659d758fc7ab96a7a0b
Rafael de Almeida Alves <rafael.almeida.alves.2002@gmail.com>
2022-10-04 18:18:34 -0300

    adicionado a cor preta como selecionada ao abrir o site

--- script.js
+++ script.js
@@ -39,14 +39,27 @@ randomButton.addEventListener('click', function () {
         pixel.style.backgroundColor = 'white'
         pixel.style.border = '1px solid black'
         pixel.style.display = 'inline-block'
-        pixel.style.width = '50px'
-        pixel.style.height = '50px'
-        pixel.style.textAlign = 'right'
+        pixel.style.width = '40px'
+        pixel.style.height = '40px'
+        pixel.style.textAlign = 'center'
         
 
         quadro.appendChild(pixel)
     }
 
+    document.getElementsByClassName('color')[0].className = 'color selected'
+    let paleta = document.getElementById('color-palette')
+    paleta.addEventListener('click', function(event){
+        if (document.getElementsByClassName('selected').length < 1) {
+        event.target.className = 'color selected'
+    }   else if (document.getElementsByClassName('selected')[0].className == event.target.className) {
+        event.target.className = 'color'
+    }   else {
+        document.getElementsByClassName('selected')[0].className = 'color'
+        event.target.className = 'color selected'
+    }
+
+})
 
 
 
@@ -58,6 +71,21 @@ randomButton.addEventListener('click', function () {
 
 
 
+
+    for(index = 0; index < document.getElementsByClassName('pixel').length; index += 1){
+    let selectedColor = document.getElementsByClassName('selected')[0]
+    let pixelButton = document.getElementsByClassName('pixel')[index]
+    pixelButton.addEventListener('click', function(event){
+        event.target.style.backgroundColor = selectedColor
+    }) 
+        if (selectedPixel !== undefined){
+    let selectedColor = selectedPixel.style.backgroundColor 
+    let pixelButton = document.getElementsByClassName('pixel')[index]
+    pixelButton.addEventListener('click', function(event){
+        event.target.style.backgroundColor = selectedColor
+      })
+    }
+  }
 
 
 
@@ -67,5 +95,4 @@ window.onload = function () {
 document.getElementsByClassName('color')[1].style.backgroundColor = JSON.parse(localStorage.getItem('colorPalette'))[0],
 document.getElementsByClassName('color')[2].style.backgroundColor = JSON.parse(localStorage.getItem('colorPalette'))[1],
 document.getElementsByClassName('color')[3].style.backgroundColor = JSON.parse(localStorage.getItem('colorPalette'))[2]
-}
-}
+}}
